72714c936430e43aaace3e51a74c1205a39f97c1
minmingsheng <shengminming1990@gmail.com>
2016-03-23 00:09:41 -0400

    change dot animation

--- app/socket/login.js
+++ app/socket/login.js
@@ -5,9 +5,9 @@ module.exports = (io,app,sid,socket)=>{
 		console.log("successfully connect with login");
 		socket.on("join", ()=>{
 			console.log("joining");
-			socket.emit("register", socket.id.replace('/login#','').split("").splice(0,2,'A').join('')  );
-			if(!id.has(socket.id.replace('/login#','').split("").splice(0,2,'A').join('')  )){
-				id.add(socket.id.replace('/login#','').split("").splice(0,2,'A').join('')  )
+			socket.emit("register", socket.id.split("").splice(4,7).concat('A').join('').toLowerCase().replace("#", 'A')  );
+			if(!id.has(socket.id.split("").splice(4,7).concat('A').join('').toLowerCase().replace("#", 'A')  )){
+				id.add(socket.id.split("").splice(4,7).concat('A').join('').toLowerCase().replace("#", 'A')  )
 			}
 			console.log("sid: ", id.size);
 			socket.join('110a')
@@ -24,24 +24,27 @@ module.exports = (io,app,sid,socket)=>{
 		});
 
 		socket.on("mousemove", (data)=>{
-			console.log("id: ", socket.id.replace('/login#','').split("").splice(0,2,'A').join('')  );
+			console.log("id: ", socket.id.split("").splice(4,7).concat('A').join('').toLowerCase().replace("#", 'A')  );
 			console.log("x: ", data.x);
 			console.log("y: ", data.y);
+			let send= ()=>{
+				socket.emit("someOneMoving", {
+					id: socket.id.split("").splice(4,7).concat('A').join('').toLowerCase().replace("#", 'A')  ,
+					x: data.x,
+					y: data.y,
+				});
+			}
 			socket.broadcast.to("110a").emit("someOneMoving", {
-				id: socket.id.replace('/login#','').split("").splice(0,2,'A').join('')  ,
+				id: socket.id.split("").splice(4,7).concat('A').join('').toLowerCase().replace("#", 'A')  ,
 				x: data.x,
 				y: data.y,
 			});
-			socket.emit("someOneMoving", {
-				id: socket.id.replace('/login#','').split("").splice(0,2,'A').join('')  ,
-				x: data.x,
-				y: data.y,
-			});
+			setTimeout(send,100);
 		})
 
 		socket.on("disconnect", ()=>{
-			if(id.has(socket.id.replace('/login#','').split("").splice(0,2,'A').join('')  )){
-				id.delete(socket.id.replace('/login#','').split("").splice(0,2,'A').join('')  )
+			if(id.has(socket.id.split("").splice(4,7).concat('A').join('').toLowerCase().replace("#", 'A')  )){
+				id.delete(socket.id.split("").splice(4,7).concat('A').join('').toLowerCase().replace("#", 'A')  )
 			}
 			socket.broadcast.to("110a").emit("countOdId", {
 				ids:  Array.from(id)
